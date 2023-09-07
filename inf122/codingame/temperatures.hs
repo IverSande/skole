5db55f5d65f1466af8bee0daad9d49044199e5ff
Iver <iversande@gmail.com>
2023-09-07 14:38:21 +0200

    codingameOppgaveTemp

--- inf122/codingame/temperatures.hs
+++ inf122/codingame/temperatures.hs
@@ -1,7 +1,21 @@
-main = interact $ show . findClosestToZero . lines 
 
-findClosestToZero :: [String] -> String 
-findClosestToZero a = show $ findClosestToZeroString (a !! 1)
+import System.IO
+import Control.Monad
+import Data.List
+import Data.Function
 
-findClosestToZeroString :: String -> Integer
-findClosestToZeroString a =  (map read(map (\x -> [x]) a) :: [Integer]) !! 1
+--Takes the input in two lines
+main = interact $ show . findClosestToZero . readToInt . head . tail . lines 
+
+readToInt :: String -> [Integer]
+readToInt a = map read $ words a :: [Integer]
+
+findClosestToZero :: [Integer] -> Integer
+findClosestToZero a = case (a /= []) of
+    True -> minimumBy (compare `on` distanceToZero ) a 
+    False -> 0
+
+distanceToZero :: Integer -> Integer
+distanceToZero a = case (a < 0) of 
+  True -> abs a + 1
+  False -> a
