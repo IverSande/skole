431028df8fd7de7741bffdafcb7c133a8a362258
IverSande <iversande@gmail.com>
2023-11-07 22:40:25 +0100

    started grading

---
+++ dat103/studentGrading/oblig2/oblig2Group150/oblig2-group150.grading/printsumTest.asm
@@ -0,0 +1,169 @@
+; Inndata programmet leser inn to sifre skilt med ett eller flere mellomrom
+; Utdata programmet skriver ut summen av de to sifrene,
+; forutsatt at summen er mindre enn 10.
+
+; Konstanter
+
+	cr 	  equ 13 ; Vognretur
+	lf 	  equ 10 ; linjeskift
+	SYS_EXIT  equ 1
+	SYS_READ  equ 3
+	SYS_WRITE equ 4
+	STDIN 	  equ 0
+	STDOUT 	  equ 1
+	STDERR	  equ 2
+
+;Datasegment
+section .bss
+	siffer	  resb 4
+
+;Datasegment
+section .data
+	meld db "Skriv to ensifrede tall med mellomrom.",cr,lf
+	     db "Summen av tallene maa vaere mindre enn 10.",cr,lf
+	meldlen equ $ - meld
+	feilmeld db cr,lf, 	"Skriv kun sifre!",cr,lf
+	feillen equ $ - feilmeld
+	crlf db cr,lf
+	crlflen equ $ - crlf
+
+; Kodesegment med program
+section .text
+
+global _start
+_start:
+	mov edx,meldlen
+	mov ecx,meld
+	mov ebx,STDOUT
+	mov eax,SYS_WRITE
+	int 80h
+
+	; Les tall, innlest tall returneres i exc
+	; Vellykket retur dersom edx = 0
+	
+	call lessiffer
+	cmp edx,0	;Test om vellykket innlesning
+	jne Slutt	;Hopp til avslutning ved feil i innlesing
+	mov eax,ecx	;Foerste tall/siffer lagres i reg eax
+
+	;Les andre tall/siffer
+	;Velllykket: edx=0, tall i ecx
+
+	call lessiffer
+	cmp edx,0 	;Test om vellykket innlesning
+	jne Slutt
+	mov ebx,ecx	;Andre tall/siffer lagres i reg ebx
+	
+	add eax, ebx
+	cmp eax, 10
+	jle Slutt
+	cmp eax, 18
+	jg Slutt
+
+	mov ecx,eax
+	call skrivsiffer ;Skriv ut verdi i ecx som ensifret tall
+	call nylinje
+
+Slutt:
+	mov eax,SYS_EXIT
+	mov ebx,0
+	int 80h
+
+;---------------------------------------------------
+skrivsiffer:
+    ;Skriver ut sifferet lagret i ecx. Ingen sjekk paa verdiomraade
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    mov eax, ecx
+    xor edx, edx
+    mov ebx, 10
+    div ebx
+
+	; lagre koefisient og rest i minne
+	mov [siffer], al
+	mov [siffer+1], dl
+
+    ; printer tierplass / koefisient 
+    add byte [siffer], '0'
+	mov ecx, siffer
+	mov edx, 1
+    mov ebx, STDOUT
+    mov eax, SYS_WRITE
+    int 80h
+
+    ; printer enerplass / rest
+    add byte [siffer+1], '0'
+    mov ecx, siffer+1
+    mov edx, 1
+    mov ebx, STDOUT
+    mov eax, SYS_WRITE
+    int 80h
+
+    ; resturerer stack
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
+;---------------------------------------------------
+lessiffer:
+	;Leter forbi alle blanke til neste ikke-blanke
+	;NEste ikke-blank returneres i ecx
+	push eax
+	push ebx
+
+Lokke:
+	;Leser et tegn fra tastaturet
+	mov eax,SYS_READ
+	mov ebx,STDIN
+	mov ecx,siffer
+	mov edx,1
+	int 80h
+	mov ecx,[siffer]
+
+	cmp ecx,' '
+	je Lokke
+	cmp ecx,'0'	;Sjekk at tast er i omraade 0-9
+	jb Feil
+	cmp ecx,'9'
+	ja Feil
+	sub ecx,'0'	;Konverter ascii til tall.
+	mov edx,0	;Signaliser vellykket innlesing
+	pop ebx
+	pop eax
+	ret			;Vellykket retur
+
+Feil:
+	mov edx,feillen
+	mov ecx,feilmeld
+	mov ebx,STDERR
+	mov eax,SYS_WRITE
+	int 80h
+	mov edx,1	;Singaliser mislykket innlesning av tall
+	pop ebx
+	pop eax
+	ret			;Mislykket retur
+
+;-----------------------------------------------------
+
+;Flytt cursor helt til venstre paa neste linje
+nylinje:
+	push eax
+	push ebx
+	push ecx
+	push edx
+	mov edx,crlflen
+	mov ecx,crlf
+	mov ebx,STDOUT
+	mov eax,SYS_WRITE
+	int 80h
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+
+;End _start
---
+++ dat103/studentGrading/oblig2/oblig2Group62/oblig2-group62.grading/printsumTest.asm
@@ -0,0 +1,178 @@
+; Inndata Programmet leser inn to sifre skilt med ett eller flere mellomrom
+; Utdata Programmet skriver ut summen av de to sifrene ,
+; forutsatt at summen er mindre enn 10.
+
+; Konstanter
+	cr equ 13 ; Vognretur
+	lf equ 10 ; Linjeskift
+	SYS_EXIT	equ 1
+	SYS_READ	equ 3
+	SYS_WRITE	equ 4
+	STDIN		equ 0
+	STDOUT		equ 1
+	STDERR		equ 2
+
+; Datasegment
+section .bss
+	siffer resb 4
+
+; Datasegment
+section .data
+	meld db "Skriv to ensifrede tall skilt med mellomrom.",cr,lf
+	     db "Summen av tallene maa vaere storre enn 10 og mindre enn 18.",cr,lf
+	meldlen equ $ - meld
+	feilmeld db cr,lf, "Skriv kun sifre!",cr,lf
+	feillen equ $ - feilmeld
+; Task 2---------------------------
+	feilmeld2 db cr,lf, "Tall er 10 eller mindre eller 18 eller storre.",cr,lf
+	feillen2 equ $ - feilmeld2
+; ---------------------------------	
+	crlf db cr,lf
+	crlflen equ $ - crlf
+
+; Kodesegment med program
+section .text
+
+global _start
+_start:
+	mov edx,meldlen
+	mov ecx,meld
+	mov ebx,STDOUT
+	mov eax,SYS_WRITE
+	int 80h
+
+	; Les tall, innlest tall returneres i ecx
+	; Vellykket retur dersom edx=0
+	call lessiffer
+	cmp edx,0	; Test om vellykket innlesning
+	jne Slutt	; Hopp til avslutning ved feil i innlesing
+	mov eax,ecx	; Foerste tall/siffer lagres i reg eax
+
+	; Les andre tall/siffer
+	; vellykket: edx=0, tall  ecx
+	call lessiffer
+	cmp edx,0	; Test om vellykket innlesning
+	jne Slutt
+	mov ebx,ecx	; andre tall/siffer lagres i reg ebx
+
+	call nylinje
+	add eax,ebx
+	mov ecx,eax
+	
+; Task 2
+	cmp ecx,11 ; Sjekke at verdien vaar ligger mellom 10 og 17
+	jb Feiltotal
+	cmp ecx,17
+	ja Feiltotal
+	call skrivsiffer ; Skriv ut verdi i ecx som ensifret tall
+
+Slutt:
+	mov eax,SYS_EXIT
+	mov ebx,0
+	int 80h
+
+; Task 2
+; --------------------------------------------------------------------
+Feiltotal:
+	mov edx,feillen2
+	mov ecx,feilmeld2
+	mov ebx,STDERR
+	mov eax,SYS_WRITE
+        int 80h
+        mov edx,1
+        pop ebx
+        pop eax
+        ret 
+; --------------------------------------------------------------------
+skrivsiffer:
+	; Skriver ut sifferet lagret i ecx. Ingen sjekk paa verdiomraade
+	push eax
+	push ebx
+	push edx
+
+	mov ebx,ecx
+
+	push ecx
+
+	shr ebx,8
+	add ebx,'0'
+	mov [siffer],ebx
+	mov ecx,siffer
+	mov ebx,STDOUT
+	mov edx,1
+	mov eax,SYS_WRITE
+	int 80h
+
+	pop ecx
+
+	add ecx,'0' ; converter tall til ascii
+	mov [siffer],ecx
+	mov ecx,siffer
+	mov edx,1
+	mov ebx,STDOUT
+	mov eax,SYS_WRITE
+	int 80h
+
+	pop edx
+	pop ebx
+	pop eax
+	ret
+
+; ---------------------------------------------------------------------
+lessiffer:
+	; Leter forbi alle blanke til neste ikke-blank
+	; Neste ikke-blank returneres i ecx
+	push eax
+	push ebx
+
+ Lokke:
+	; Leser et tegn fra tastaturet
+	mov eax,SYS_READ
+	mov ebx,STDIN
+	mov ecx,siffer
+	mov edx,1
+	int 80h
+	mov ecx,[siffer]
+
+	cmp ecx,' '
+	je Lokke
+	cmp ecx,'0' ; Sjekk at tast er i omraade 0-9
+	jb Feil
+	cmp ecx,'9'
+	ja Feil
+	sub ecx,'0' ; Konverterer ascii til et tall.
+	mov edx,0   ; signaliserer vellykket innlesning
+	pop ebx
+	pop eax
+	ret	    ; vellykket retur
+
+ Feil:
+	mov edx,feillen
+	mov ecx,feilmeld
+	mov ebx,STDERR
+	mov eax,SYS_WRITE
+	int 80h
+	mov edx,1	; Signaliserer mislykket innlesning av tall
+	pop ebx
+	pop eax
+	ret		; Mislykket retur
+
+;----------------------------------------------------------------------
+; Flytt cursor helt til venstre paa neste linje
+nylinje:
+	push eax
+	push ebx
+	push ecx
+	push edx
+	mov edx,crlflen
+	mov ecx,crlf
+	mov ebx,STDOUT
+	mov eax,SYS_WRITE
+	int 80h
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+
+; End _start
